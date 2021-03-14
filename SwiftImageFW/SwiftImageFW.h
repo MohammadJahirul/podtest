d6ef1c7c09995e9cffdc31fa135939f75c76638c
Mohammad Jahir <>
2021-03-14 19:37:47 +0600

    with project

---
+++ SwiftImageFW/SwiftImageFW.h
@@ -0,0 +1,18 @@
+//
+//  SwiftImageFW.h
+//  SwiftImageFW
+//
+//  Created by Mohammad Jahir on 3/14/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftImageFW.
+FOUNDATION_EXPORT double SwiftImageFWVersionNumber;
+
+//! Project version string for SwiftImageFW.
+FOUNDATION_EXPORT const unsigned char SwiftImageFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftImageFW/PublicHeader.h>
+
+
